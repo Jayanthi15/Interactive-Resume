5bc01c531cb4d072ef78c8cc77b07b0d5e4d82a9
Jayanthi15 <jayanthi1592@gmail.com>
2017-02-02 01:48:16 +0530

    Html | CSS | JS Beautified

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,8 +2,7 @@
  * education object contain data about the schools and onlineCourses
  */
 var education = {
-    schools: [
-        {
+    schools: [{
             name: 'VRS College',
             location: 'Villupuram',
             degree: 'BE',
@@ -20,8 +19,7 @@ var education = {
             majors: ['CompSci']
         }
     ],
-    onlineCourses: [
-        {
+    onlineCourses: [{
             title: 'Intro to HTML and CSS',
             school: 'Udacity',
             date: 2005,
@@ -39,8 +37,7 @@ var education = {
  * work object contain data about the jobs
  */
 var work = {
-    jobs: [
-        {
+    jobs: [{
             employer: 'Infosys',
             title: 'Web Developer',
             location: 'Bangalore',
@@ -60,8 +57,7 @@ var work = {
  * projects object cotains data about the projects
  */
 var projects = {
-    projects: [
-        {
+    projects: [{
             title: 'Animal Trading Cards',
             dates: '2011 ',
             description: 'This project is simply contains the HTML,CSS. Designing card about the description animal in which HTML contains the contains about animal scientific name, mass, lifespan, other details & their structures of the page are described. CSS used to enhance the styles',
@@ -79,17 +75,17 @@ var projects = {
  * bio object contains data about biography
  */
 var bio = {
-        name: 'Jayanthi Vijayan',
-        role: 'Web Developer',
-        welcomeMessage: 'Hi! I am proactive quick learner & crazy tech greek. Love to do Programming Challenges' ,
-        biopic: 'images/biopic_1.jpg',
-        contacts: {
-            mobile: '9894155825',
-            email: 'jayanthi1592@gmail.com',
-            github: 'https://github.com/jayanthi',
-            location: 'Chennai'
-        },
-        skills: ['Teaching', 'JS', 'HTML', 'css','Programming']
+    name: 'Jayanthi Vijayan',
+    role: 'Web Developer',
+    welcomeMessage: 'Hi! I am proactive quick learner & crazy tech greek. Love to do Programming Challenges',
+    biopic: 'images/biopic_1.jpg',
+    contacts: {
+        mobile: '9894155825',
+        email: 'jayanthi1592@gmail.com',
+        github: 'https://github.com/jayanthi',
+        location: 'Chennai'
+    },
+    skills: ['Teaching', 'JS', 'HTML', 'css', 'Programming']
 };
 /**
  *  internationalizeButton contains the html for button 
@@ -105,23 +101,23 @@ $('#mapDiv').append(googleMap);
  * bio.display() is a function attached to the bio object which is used to 
  * display biography data in UI/FrontEnd.
  */
-bio.display=function(){
-    $('#header').prepend(HTMLheaderRole.replace('%data%',bio.role));
-    $('#header').prepend(HTMLheaderName.replace('%data%',bio.name));
-    $('#topContacts,#footerContacts').append(HTMLmobile.replace('%data%',bio.contacts.mobile));
-    $('#topContacts,#footerContacts').append(HTMLemail.replace('%data%',bio.contacts.email));
-    $('#topContacts,#footerContacts').append(HTMLgithub.replace('%data%',bio.contacts.github));
-    $('#topContacts,#footerContacts').append(HTMLlocation.replace('%data%',bio.contacts.location));
-    $('#header').append(HTMLbioPic.replace('%data%',bio.biopic).replace('%altdata%',bio.name));
-    $('#header').append(HTMLwelcomeMsg.replace('%data%',bio.welcomeMessage));
-    if(bio.skills.length>0){
+bio.display = function() {
+    $('#header').prepend(HTMLheaderRole.replace('%data%', bio.role));
+    $('#header').prepend(HTMLheaderName.replace('%data%', bio.name));
+    $('#topContacts,#footerContacts').append(HTMLmobile.replace('%data%', bio.contacts.mobile));
+    $('#topContacts,#footerContacts').append(HTMLemail.replace('%data%', bio.contacts.email));
+    $('#topContacts,#footerContacts').append(HTMLgithub.replace('%data%', bio.contacts.github));
+    $('#topContacts,#footerContacts').append(HTMLlocation.replace('%data%', bio.contacts.location));
+    $('#header').append(HTMLbioPic.replace('%data%', bio.biopic).replace('%altdata%', bio.name));
+    $('#header').append(HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage));
+    if (bio.skills.length > 0) {
         $('#header').append(HTMLskillsStart);
         /**
-        * Iterates through all the skills in bio object and append them as a 
-        * children to '#skills' container
-        */
-        bio.skills.forEach(function(value,index){
-            $('#skills').append(HTMLskills.replace('%data%',value));
+         * Iterates through all the skills in bio object and append them as a 
+         * children to '#skills' container
+         */
+        bio.skills.forEach(function(value, index) {
+            $('#skills').append(HTMLskills.replace('%data%', value));
         });
     }
 };
@@ -129,19 +125,19 @@ bio.display=function(){
  * work.display() is a function attached to the work object which is used to 
  * display work experience data on UI/FrontEnd.
  */
-work.display=function(){
+work.display = function() {
     /**
      * Iterates through all the jobs and append them as a children to 
      * '.work-entry' container
      */
-    work.jobs.forEach(function(value,index){
+    work.jobs.forEach(function(value, index) {
         $('#workExperience').append(HTMLworkStart);
-        var formattedEmployer=HTMLworkEmployer.replace('%data%',value.employer);
-        var formattedTitle=HTMLworkTitle.replace('%data%',value.title);
-        var formattedWorkDates=HTMLworkDates.replace('%data%',value.dates);
-        var formattedWorkLocation=HTMLworkLocation.replace('%data%',value.location);
-        var formattedWorkDescription=HTMLworkDescription.replace('%data%',value.description);
-        var formattedEmployerTitle=formattedEmployer+formattedTitle+formattedWorkDates+formattedWorkLocation+formattedWorkDescription;
+        var formattedEmployer = HTMLworkEmployer.replace('%data%', value.employer);
+        var formattedTitle = HTMLworkTitle.replace('%data%', value.title);
+        var formattedWorkDates = HTMLworkDates.replace('%data%', value.dates);
+        var formattedWorkLocation = HTMLworkLocation.replace('%data%', value.location);
+        var formattedWorkDescription = HTMLworkDescription.replace('%data%', value.description);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle + formattedWorkDates + formattedWorkLocation + formattedWorkDescription;
         $('.work-entry:last').append(formattedEmployerTitle);
     });
 };
@@ -149,67 +145,67 @@ work.display=function(){
  * projects.display() is a function attached to the projects object which is used to 
  * display projects data on UI/FrontEnd.
  */
-projects.display=function(){
+projects.display = function() {
     /**
      * Iterates through all the projects and append them as a children to 
      * '.project-entry' container
      */
-    projects.projects.forEach(function(value,index){
+    projects.projects.forEach(function(value, index) {
         $('#projects').append(HTMLprojectStart);
-        var formattedProjectTitle=HTMLprojectTitle.replace('%data%',value.title);
-        var formattedProjectDates=HTMLprojectDates.replace('%data%',value.dates);
-        var formattedProjectDescription=HTMLprojectDescription.replace('%data%',value.description);
-        var formattedProjectimages='';
-        value.images.forEach(function(image_value,image_index){
-            formattedProjectimages+=HTMLprojectImage.replace('%data%',image_value).replace('%altdata%',value.title);
+        var formattedProjectTitle = HTMLprojectTitle.replace('%data%', value.title);
+        var formattedProjectDates = HTMLprojectDates.replace('%data%', value.dates);
+        var formattedProjectDescription = HTMLprojectDescription.replace('%data%', value.description);
+        var formattedProjectimages = '';
+        value.images.forEach(function(image_value, image_index) {
+            formattedProjectimages += HTMLprojectImage.replace('%data%', image_value).replace('%altdata%', value.title);
         });
-        $('.project-entry:last').append(formattedProjectTitle+formattedProjectDates+formattedProjectDescription+formattedProjectimages);
+        $('.project-entry:last').append(formattedProjectTitle + formattedProjectDates + formattedProjectDescription + formattedProjectimages);
     });
 };
 /**
  * education.display() is a function attached to the education object which is used to 
  * display education data like schools and onlinecources on UI/FrontEnd.
  */
-education.display=function(){
+education.display = function() {
     /**
      * Iterates through all the schools and append them as a children to 
      * '.education-entry' container
      */
-    education.schools.forEach(function(value,index){
+    education.schools.forEach(function(value, index) {
         $('#education').append(HTMLschoolStart);
-        var formattedSchoolName=HTMLschoolName.replace('%data%',value.name);
-        var formattedSchoolDegree=HTMLschoolDegree.replace('%data%',value.degree);
-        var formattedSchoolDates=HTMLschoolDates.replace('%data%',value.dates);
-        var formattedSchoolLocation=HTMLschoolLocation.replace('%data%',value.location);
-        var formattedSchoolMajor=HTMLschoolMajor.replace('%data%',value.majors);
-        $('.education-entry:last').append(formattedSchoolName+formattedSchoolDegree+formattedSchoolDates+formattedSchoolLocation+formattedSchoolMajor);
+        var formattedSchoolName = HTMLschoolName.replace('%data%', value.name);
+        var formattedSchoolDegree = HTMLschoolDegree.replace('%data%', value.degree);
+        var formattedSchoolDates = HTMLschoolDates.replace('%data%', value.dates);
+        var formattedSchoolLocation = HTMLschoolLocation.replace('%data%', value.location);
+        var formattedSchoolMajor = HTMLschoolMajor.replace('%data%', value.majors);
+        $('.education-entry:last').append(formattedSchoolName + formattedSchoolDegree + formattedSchoolDates + formattedSchoolLocation + formattedSchoolMajor);
     });
     $('#education').append(HTMLonlineClasses);
     /**
      * Iterates through all the onlineCourses and append them as a children to
      * '.education-entry' container
      */
-    education.onlineCourses.forEach(function(value,index){
+    education.onlineCourses.forEach(function(value, index) {
         $('#education').append(HTMLschoolStart);
-        var formattedOnlineCoursesTitle=HTMLonlineTitle.replace('%data%',value.title);
-        var formattedOnlineCoursesSchool=HTMLonlineSchool.replace('%data%',value.school);
-        var formattedOnlineCoursesDates=HTMLonlineDates.replace('%data%',value.date);
-        var formattedOnlineCoursesURL=HTMLonlineURL.replace('%data%',value.url);
-        $('.education-entry:last').append(formattedOnlineCoursesTitle+formattedOnlineCoursesSchool+formattedOnlineCoursesDates+formattedOnlineCoursesURL);
+        var formattedOnlineCoursesTitle = HTMLonlineTitle.replace('%data%', value.title);
+        var formattedOnlineCoursesSchool = HTMLonlineSchool.replace('%data%', value.school);
+        var formattedOnlineCoursesDates = HTMLonlineDates.replace('%data%', value.date);
+        var formattedOnlineCoursesURL = HTMLonlineURL.replace('%data%', value.url);
+        $('.education-entry:last').append(formattedOnlineCoursesTitle + formattedOnlineCoursesSchool + formattedOnlineCoursesDates + formattedOnlineCoursesURL);
     });
-    
+
 };
 /**
  * inName() is a function which accepts name as parameter and capitalise each word 
  * and return them as a single string.
  */
-function inName(name){
-    var splitted_name=name.split(' ');
-    var first=splitted_name[0];
-    var second=splitted_name[1];
-    var formattedFirstName=first.slice(0,1).toUpperCase()+first.slice(1).toLowerCase();
-    var formattedLastName=second.slice(0).toUpperCase();
-    return formattedFirstName+' '+formattedLastName;
+function inName(name) {
+    var splitted_name = name.split(' ');
+    var first = splitted_name[0];
+    var second = splitted_name[1];
+    var formattedFirstName = first.slice(0, 1).toUpperCase() + first.slice(1).toLowerCase();
+    var formattedLastName = second.slice(0).toUpperCase();
+    return formattedFirstName + ' ' + formattedLastName;
 }
 /**
  * projects.display() is a function call which is attached in projects object
