a34c81d2e9471d1efdef2cba92593952f0ea54a9
Jayanthi15 <jayanthi1592@gmail.com>
2017-02-02 13:09:37 +0530

    Changed date to dates and its value as string | added HTMLdata and HTMLaltData variable for HTML replace string

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -22,13 +22,13 @@ var education = {
     onlineCourses: [{
             title: 'Intro to HTML and CSS',
             school: 'Udacity',
-            date: 2005,
+            dates: '2005',
             url: 'https://classroom.udacity.com/courses/ud304/'
         },
         {
             title: 'JavaScript basics',
             school: 'Udacity',
-            date: 2006,
+            dates: '2006',
             url: 'https://classroom.udacity.com/courses/ud804/'
         }
     ]
@@ -60,7 +60,7 @@ var projects = {
     projects: [{
             title: 'Animal Trading Cards',
             dates: '2011 ',
-            description: 'This project is simply contains the HTML,CSS. Designing card about the description animal in which HTML contains the contains about animal scientific name, mass, lifespan, other details & their structures of the page are described. CSS used to enhance the styles',
+            description: 'This project is simply contains the HTML, CSS. Designing card about the description of a animal in which HTML contains about the animal\'s scientific name, mass, lifespan, other details & their structures of the page are described. CSS used to enhance the styles',
             images: ['images/projects-1.jpg']
         },
         {
@@ -77,7 +77,7 @@ var projects = {
 var bio = {
     name: 'Jayanthi Vijayan',
     role: 'Web Developer',
-    welcomeMessage: 'Hi! I am proactive quick learner & crazy tech greek. Love to do Programming Challenges',
+    welcomeMessage: 'Hi! I am proactive quick learner & crazy tech geek. Love to do Programming Challenges',
     biopic: 'images/biopic_1.jpg',
     contacts: {
         mobile: '9894155825',
@@ -85,7 +85,7 @@ var bio = {
         github: 'https://github.com/jayanthi',
         location: 'Chennai'
     },
-    skills: ['Teaching', 'JS', 'HTML', 'css', 'Programming']
+    skills: ['Teaching', 'JS', 'HTML', 'CSS', 'Programming']
 };
 /**
  *  internationalizeButton contains the html for button 
@@ -101,23 +101,25 @@ $('#mapDiv').append(googleMap);
  * bio.display() is a function attached to the bio object which is used to 
  * display biography data in UI/FrontEnd.
  */
-bio.display = function() {
-    $('#header').prepend(HTMLheaderRole.replace('%data%', bio.role));
-    $('#header').prepend(HTMLheaderName.replace('%data%', bio.name));
-    $('#topContacts,#footerContacts').append(HTMLmobile.replace('%data%', bio.contacts.mobile));
-    $('#topContacts,#footerContacts').append(HTMLemail.replace('%data%', bio.contacts.email));
-    $('#topContacts,#footerContacts').append(HTMLgithub.replace('%data%', bio.contacts.github));
-    $('#topContacts,#footerContacts').append(HTMLlocation.replace('%data%', bio.contacts.location));
-    $('#header').append(HTMLbioPic.replace('%data%', bio.biopic).replace('%altdata%', bio.name));
-    $('#header').append(HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage));
-    if (bio.skills.length > 0) {
+var HTMLdata = '%data%';
+var HTMLaltData = '%altdata%';
+bio.display=function(){
+    $('#header').prepend(HTMLheaderRole.replace(HTMLdata,bio.role));
+    $('#header').prepend(HTMLheaderName.replace(HTMLdata,bio.name));
+    $('#topContacts,#footerContacts').append(HTMLmobile.replace(HTMLdata,bio.contacts.mobile));
+    $('#topContacts,#footerContacts').append(HTMLemail.replace(HTMLdata,bio.contacts.email));
+    $('#topContacts,#footerContacts').append(HTMLgithub.replace(HTMLdata,bio.contacts.github));
+    $('#topContacts,#footerContacts').append(HTMLlocation.replace(HTMLdata,bio.contacts.location));
+    $('#header').append(HTMLbioPic.replace(HTMLdata,bio.biopic).replace(HTMLaltData,bio.name));
+    $('#header').append(HTMLwelcomeMsg.replace(HTMLdata,bio.welcomeMessage));
+    if(bio.skills.length>0){
         $('#header').append(HTMLskillsStart);
         /**
-         * Iterates through all the skills in bio object and append them as a 
-         * children to '#skills' container
-         */
-        bio.skills.forEach(function(value, index) {
-            $('#skills').append(HTMLskills.replace('%data%', value));
+        * Iterates through all the skills in bio object and append them as a 
+        * children to '#skills' container
+        */
+        bio.skills.forEach(function(value,index){
+            $('#skills').append(HTMLskills.replace(HTMLdata,value));
         });
     }
 };
@@ -130,14 +132,14 @@ work.display = function() {
      * Iterates through all the jobs and append them as a children to 
      * '.work-entry' container
      */
-    work.jobs.forEach(function(value, index) {
+    work.jobs.forEach(function(value,index){
         $('#workExperience').append(HTMLworkStart);
-        var formattedEmployer = HTMLworkEmployer.replace('%data%', value.employer);
-        var formattedTitle = HTMLworkTitle.replace('%data%', value.title);
-        var formattedWorkDates = HTMLworkDates.replace('%data%', value.dates);
-        var formattedWorkLocation = HTMLworkLocation.replace('%data%', value.location);
-        var formattedWorkDescription = HTMLworkDescription.replace('%data%', value.description);
-        var formattedEmployerTitle = formattedEmployer + formattedTitle + formattedWorkDates + formattedWorkLocation + formattedWorkDescription;
+        var formattedEmployer=HTMLworkEmployer.replace(HTMLdata,value.employer);
+        var formattedTitle=HTMLworkTitle.replace(HTMLdata,value.title);
+        var formattedWorkDates=HTMLworkDates.replace(HTMLdata,value.dates);
+        var formattedWorkLocation=HTMLworkLocation.replace(HTMLdata,value.location);
+        var formattedWorkDescription=HTMLworkDescription.replace(HTMLdata,value.description);
+        var formattedEmployerTitle=formattedEmployer+formattedTitle+formattedWorkDates+formattedWorkLocation+formattedWorkDescription;
         $('.work-entry:last').append(formattedEmployerTitle);
     });
 };
@@ -150,16 +152,16 @@ projects.display = function() {
      * Iterates through all the projects and append them as a children to 
      * '.project-entry' container
      */
-    projects.projects.forEach(function(value, index) {
+    projects.projects.forEach(function(value,index){
         $('#projects').append(HTMLprojectStart);
-        var formattedProjectTitle = HTMLprojectTitle.replace('%data%', value.title);
-        var formattedProjectDates = HTMLprojectDates.replace('%data%', value.dates);
-        var formattedProjectDescription = HTMLprojectDescription.replace('%data%', value.description);
-        var formattedProjectimages = '';
-        value.images.forEach(function(image_value, image_index) {
-            formattedProjectimages += HTMLprojectImage.replace('%data%', image_value).replace('%altdata%', value.title);
+        var formattedProjectTitle=HTMLprojectTitle.replace(HTMLdata,value.title);
+        var formattedProjectDates=HTMLprojectDates.replace(HTMLdata,value.dates);
+        var formattedProjectDescription=HTMLprojectDescription.replace(HTMLdata,value.description);
+        var formattedProjectimages='';
+        value.images.forEach(function(image_value,image_index){
+            formattedProjectimages+=HTMLprojectImage.replace(HTMLdata,image_value).replace(HTMLaltData,value.title);
         });
-        $('.project-entry:last').append(formattedProjectTitle + formattedProjectDates + formattedProjectDescription + formattedProjectimages);
+        $('.project-entry:last').append(formattedProjectTitle+formattedProjectDates+formattedProjectDescription+formattedProjectimages);
     });
 };
 /**
@@ -171,27 +173,27 @@ education.display = function() {
      * Iterates through all the schools and append them as a children to 
      * '.education-entry' container
      */
-    education.schools.forEach(function(value, index) {
+    education.schools.forEach(function(value,index){
         $('#education').append(HTMLschoolStart);
-        var formattedSchoolName = HTMLschoolName.replace('%data%', value.name);
-        var formattedSchoolDegree = HTMLschoolDegree.replace('%data%', value.degree);
-        var formattedSchoolDates = HTMLschoolDates.replace('%data%', value.dates);
-        var formattedSchoolLocation = HTMLschoolLocation.replace('%data%', value.location);
-        var formattedSchoolMajor = HTMLschoolMajor.replace('%data%', value.majors);
-        $('.education-entry:last').append(formattedSchoolName + formattedSchoolDegree + formattedSchoolDates + formattedSchoolLocation + formattedSchoolMajor);
+        var formattedSchoolName=HTMLschoolName.replace(HTMLdata,value.name);
+        var formattedSchoolDegree=HTMLschoolDegree.replace(HTMLdata,value.degree);
+        var formattedSchoolDates=HTMLschoolDates.replace(HTMLdata,value.dates);
+        var formattedSchoolLocation=HTMLschoolLocation.replace(HTMLdata,value.location);
+        var formattedSchoolMajor=HTMLschoolMajor.replace(HTMLdata,value.majors);
+        $('.education-entry:last').append(formattedSchoolName+formattedSchoolDegree+formattedSchoolDates+formattedSchoolLocation+formattedSchoolMajor);
     });
     $('#education').append(HTMLonlineClasses);
     /**
      * Iterates through all the onlineCourses and append them as a children to
      * '.education-entry' container
      */
-    education.onlineCourses.forEach(function(value, index) {
+    education.onlineCourses.forEach(function(value,index){
         $('#education').append(HTMLschoolStart);
-        var formattedOnlineCoursesTitle = HTMLonlineTitle.replace('%data%', value.title);
-        var formattedOnlineCoursesSchool = HTMLonlineSchool.replace('%data%', value.school);
-        var formattedOnlineCoursesDates = HTMLonlineDates.replace('%data%', value.date);
-        var formattedOnlineCoursesURL = HTMLonlineURL.replace('%data%', value.url);
-        $('.education-entry:last').append(formattedOnlineCoursesTitle + formattedOnlineCoursesSchool + formattedOnlineCoursesDates + formattedOnlineCoursesURL);
+        var formattedOnlineCoursesTitle=HTMLonlineTitle.replace(HTMLdata,value.title);
+        var formattedOnlineCoursesSchool=HTMLonlineSchool.replace(HTMLdata,value.school);
+        var formattedOnlineCoursesDates=HTMLonlineDates.replace(HTMLdata,value.dates);
+        var formattedOnlineCoursesURL=HTMLonlineURL.replace(HTMLdata,value.url);
+        $('.education-entry:last').append(formattedOnlineCoursesTitle+formattedOnlineCoursesSchool+formattedOnlineCoursesDates+formattedOnlineCoursesURL);
     });
 
 };
